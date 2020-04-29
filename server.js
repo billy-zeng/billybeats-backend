bcf9567e55f9b5bf2c7c48aea5d44e394a31f460
billy-zeng <bzeng95@gmail.com>
2020-04-29 00:28:17 -0700

    sets up base server file

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const cors = require('cors');
+const app = express();
+
+require('dotenv').config();
+const PORT = process.env.PORT
+
+const routes = require('./routes');
+
+// --------------------------------- Middleware --------------------------------- //
+
+// cors
+const corsOptions = {
+  origin: ['http://localhost:3000'],
+  credentials: true,
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+
+// BodyParser
+app.use(bodyParser.json());
+
+// Express Session
+app.use(
+  session({
+    store: new MongoStore({ url: process.env.MONGODB_URI }), 
+    secret: process.env.SESSION_SECRET, 
+    resave: false, 
+    saveUninitialized: false,
+    cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7 * 2 // 2 weeks
+    }
+  })
+);
+
+// ----------------------------------- Routes ----------------------------------- //
+
+
+// ----------------------------------- Start Server ----------------------------------- //
+app.listen(PORT, () =>
+  console.log(`Server connected at http://localhost:${PORT}`)
+);
