4366a357767ec565b72d47e65fc38e392e4a850f
billy-zeng <bzeng95@gmail.com>
2020-05-01 16:24:59 -0700

    adds user routes

--- controllers/users.js
+++ controllers/users.js
@@ -26,6 +26,32 @@ const edit = (req, res) => {
   });
 };
 
+// follow an artist
+const addArtist = async (req, res) => {
+  try {
+    const updatedUser = await db.User.findById(req.session.currentUser.id);
+    // const updatedUser = await db.User.findById(req.params.id);
+    updatedUser.following.push(req.params.artistId);
+    updatedUser.save();
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    return res.status(500).json({ message: "Something went wrong, try again" });
+  }
+};
+
+// unfollow an artist
+const removeArtist = async (req, res) => {
+  try {
+    let updatedUser = await db.User.findById(req.session.currentUser.id);
+    // const updatedUser = await db.User.findById(req.params.id);
+    updatedUser.players.pull({ _id: req.params.artistId })
+    updatedUser.save();
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    return res.status(500).json({ message: "Something went wrong, try again", err: err });
+  };
+};
+
 // add song to a user's likes
 const addSong = async (req, res) => {
   try {
@@ -53,32 +79,6 @@ const removeSong = async (req, res) => {
   };
 };
 
-// follow an artist
-const addArtist = async (req, res) => {
-  try {
-    const updatedUser = await db.User.findById(req.session.currentUser.id);
-    // const updatedUser = await db.User.findById(req.params.id);
-    updatedUser.following.push(req.params.artistId);
-    updatedUser.save();
-    res.status(200).json(updatedUser);
-  } catch (err) {
-    return res.status(500).json({ message: "Something went wrong, try again" });
-  }
-};
-
-// unfollow an artist
-const removeArtist = async (req, res) => {
-  try {
-    let updatedUser = await db.User.findById(req.session.currentUser.id);
-    // const updatedUser = await db.User.findById(req.params.id);
-    updatedUser.players.pull({ _id: req.params.artistId })
-    updatedUser.save();
-    res.status(200).json(updatedUser);
-  } catch (err) {
-    return res.status(500).json({ message: "Something went wrong, try again", err: err });
-  };
-};
-
 // delete user's account
 const destroy = (req, res) => {
   db.User.findByIdAndDelete(req.params.id, (err, deletedUser) => {
@@ -91,9 +91,9 @@ const destroy = (req, res) => {
 module.exports = {
   show,
   edit,
-  addSong,
-  removeSong,
   addArtist,
   removeArtist,
+  addSong,
+  removeSong,
   destroy
 };
---
+++ routes/users.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const ctrl = require('../controllers');
+
+//base route: localhost:4000/api/v1/users
+router.get('/:id', ctrl.users.show);
+router.put('/:id/following/:artistId', ctrl.users.addArtist);
+router.delete('/:id/following/:artistId', ctrl.users.removeSong);
+router.put('/:id/likes/:songId', ctrl.users.addSong);
+router.delete('/:id/likes/:songId', ctrl.users.removeSong);
+router.delete('/:id', ctrl.users.destroy)
+router.put('/:id', ctrl.users.edit);
+
+module.exports = router;
